fa08750b9361135876c70892f9271244879263e2
Genário Azevedo <genario.azevedo@ufape.edu.br>
2023-10-06 12:57:17 -0300

    Construção da página de sucesso

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 const carousel = document.querySelector(".carousel-animals");
-const prevButton = document.getElementById("prev");
-const nextButton = document.getElementById("next");
-const slideWidth = carousel.offsetWidth / 4; // Largura de 1 slide
+const prevButton = document.getElementById("left");
+const nextButton = document.getElementById("right");
+const slideWidth = carousel.offsetWidth / 4; 
 let position = 0;
 
-// Clona os primeiros slides e os adiciona ao final do carrossel
+
 const slides = Array.from(carousel.querySelectorAll(".carousel-slide"));
 const clonedSlides = slides.slice(0, 4).map(slide => slide.cloneNode(true));
 clonedSlides.forEach(clonedSlide => {
@@ -16,13 +16,12 @@ nextButton.addEventListener("click", () => {
     carousel.style.transition = "transform 0.5s ease-in-out";
     carousel.style.transform = `translateX(${position}px)`;
 
-    // Quando chegamos ao final do carrossel original, reinicie a posição
     if (position <= -slideWidth * (slides.length - 4)) {
         setTimeout(() => {
             position = 0;
             carousel.style.transition = "none";
             carousel.style.transform = `translateX(${position}px)`;
-        }, 500); // Espera a transição terminar antes de reiniciar
+        }, 500); 
     }
 });
 
@@ -31,12 +30,11 @@ prevButton.addEventListener("click", () => {
     carousel.style.transition = "transform 0.5s ease-in-out";
     carousel.style.transform = `translateX(${position}px)`;
 
-    // Quando chegamos ao início do carrossel original, vá para o final do carrossel clonado
     if (position >= 0) {
         setTimeout(() => {
             position = -slideWidth * (slides.length - 4);
             carousel.style.transition = "none";
             carousel.style.transform = `translateX(${position}px)`;
-        }, 500); // Espera a transição terminar antes de mover para o final
+        }, 500);
     }
 });
